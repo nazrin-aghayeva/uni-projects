ba589cd06553a40d737ece469569de1b049ace8c
nazrin-aghayeva <nazrinaghayeva@gmail.com>
2020-12-11 13:43:04 +0400

    -- improved

--- src/main/java/exam/PolynomialApp.java
+++ src/main/java/exam/PolynomialApp.java
@@ -3,45 +3,45 @@ package exam;
 import java.util.*;
 
 public class PolynomialApp {
-//    static List<String> result= new ArrayList<>();
-        public static void main(String[] args) {
-        String polynomial = "3x^3+4x^5+6";
-
-        String[] array = polynomial.split("\\+" );
-
-        for (String isConstantNum : array) {
-            if (isConstant(isConstantNum)) {
-                System.out.println(isConstantNum);
-            }
-        }
-    }
-
-    public static boolean isConstant(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch(NumberFormatException e){
-            return false;
-        }
-    }
-    // needed improve
-//    public static void main(String[] args) {
-//        isConstant();
-//    }
+    static List<String> result= new ArrayList<>();
+//        public static void main(String[] args) {
+//        String polynomial = "3x^3+4x^5+6";
 //
-//    public static void isConstant() {
-//        HashMap<Integer, String> function = new HashMap<>();
-//        function.put(0, "x");
-//        function.put(1, " ");
-//        function.put(2, "x");
-//        function.put(3, " ");
+//        String[] array = polynomial.split("\\+" );
 //
-//        for (int i = 0; i < function.size(); i++) {
-//            if (function.get(i).equals(" ")) {
-//                String constant= function.keySet().toArray()[i].toString();
-//                result.add(constant);
+//        for (String isConstantNum : array) {
+//            if (isConstant(isConstantNum)) {
+//                System.out.println(isConstantNum);
 //            }
 //        }
-//        System.out.println("Are constants: "+result);
 //    }
+//
+//    public static boolean isConstant(String str) {
+//        try {
+//            Double.parseDouble(str);
+//            return true;
+//        } catch(NumberFormatException e){
+//            return false;
+//        }
+//    }
+    // needed improve
+    public static void main(String[] args) {
+        isConstant();
+    }
+
+    public static void isConstant() {
+        HashMap<Integer, String> function = new HashMap<>();
+        function.put(0, "x");
+        function.put(1, "x^3");
+        function.put(2, "x");
+        function.put(3, " ");
+
+        for (int i = 0; i < function.size(); i++) {
+            if (function.get(i).equals(" ")) {
+                String constant= function.keySet().toArray()[i].toString();
+                result.add(constant);
+            }
+        }
+        System.out.println("Are constants: "+result);
+    }
 }
